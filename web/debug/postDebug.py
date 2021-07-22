04e7965a95982f020aede8f5e8611b95794c107c
CarlKuhligk <87583841+CarlKuhligk@users.noreply.github.com>
2021-07-22 16:33:12 +0200

    replaced with postSensorDataTest.py

--- web/debug/postDebug.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'http://127.0.0.1/imutracker/api/postSensorData.php'
-myData = {"api_key":"kzNABRcbVBQghFDC","accX":5.97,"accY":0.06,"accZ":6.81,"gyrX":-0.04,"gyrY":0.00,"gyrZ":0.01,"temp":33.59}
-
-x = requests.post(url, data = myData)
-
-print(myData)
-print(x.text)
