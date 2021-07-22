1da1a13809971cceaab22f4c3236379a70c2601c
CarlKuhligk <87583841+CarlKuhligk@users.noreply.github.com>
2021-07-22 11:57:22 +0200

    update localhost ip

--- web/debug/postDebug.py
+++ web/debug/postDebug.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://172.24.192.1/imutracker/api/postSensorData.php'
+url = 'http://127.0.0.1/imutracker/api/postSensorData.php'
 myData = {"api_key":"kzNABRcbVBQghFDC","accX":5.97,"accY":0.06,"accZ":6.81,"gyrX":-0.04,"gyrY":0.00,"gyrZ":0.01,"temp":33.59}
 
 x = requests.post(url, data = myData)
