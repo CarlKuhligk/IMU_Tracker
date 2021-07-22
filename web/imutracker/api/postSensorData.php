1f706289c605e0a15988c3b109935c67c992e54f
CarlKuhligk <87583841+CarlKuhligk@users.noreply.github.com>
2021-07-22 22:35:06 +0200

    updated to the new dataprocessing

--- web/imutracker/api/postSensorData.php
+++ web/imutracker/api/postSensorData.php
@@ -1,29 +1,26 @@
 <?php
-
-/*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp32-esp8266-mysql-database-php/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*/
-
-$servername = "localhost";
-$dbname = "imutracker";
-$username = "imuAPI";
-$password = "Cs.![qPtTOAVxs].";
-
-// api keys stored in the database
-$api_key_value = "kzNABRcbVBQghFDC";
+include 'databaseAccess.php';
 
 $api_key = $id = $time = $accX = $accY = $accZ = $gyrX = $gyrY = $gyrZ = $temp = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $api_key = htmlspecialchars($_POST["api_key"]);
-    if($api_key == $api_key_value) {
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT id, name FROM devices WHERE api_key = '$api_key';";
+    $result = $conn->query($sql);
+    $row = $result -> fetch_array(MYSQLI_ASSOC);
+
+    if ( $row != null) {
+        $id = $row['id'];
+        $name = $row['name'];
+        $tableName = $name."_".$id;
+
         $accX = htmlspecialchars($_POST["accX"]);
         $accY = htmlspecialchars($_POST["accY"]);
         $accZ = htmlspecialchars($_POST["accZ"]);
@@ -31,31 +28,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $gyrY = htmlspecialchars($_POST["gyrY"]);
         $gyrZ = htmlspecialchars($_POST["gyrZ"]);
         $temp = htmlspecialchars($_POST["temp"]);
-    
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        } 
-        
+
         // update always the oldest entry
-        $sql = "UPDATE measures SET accX=$accX, accY=$accY, accZ=$accZ, gyrX=$gyrX, gyrY=$gyrY, gyrZ=$gyrZ, temp=$temp WHERE timestamp = (SELECT MIN(timestamp) FROM measures) ORDER BY id LIMIT 1";
-        
+        $sql = "UPDATE $tableName SET accX=$accX, accY=$accY, accZ=$accZ, gyrX=$gyrX, gyrY=$gyrY, gyrZ=$gyrZ, temp=$temp WHERE timestamp = (SELECT MIN(timestamp) FROM $tableName) ORDER BY id LIMIT 1";
+
         if ($conn->query($sql) === TRUE) {
             echo "Entry updated successfully";
         } 
         else {
             echo "Error: " . $sql . "<br>" . $conn->error;
         }
-    
-        $conn->close();
-    }
+    } 
     else {
         echo "Wrong API Key provided.";
+        echo "Error: " . $sql . "<br>" . $conn->error;
     }
-
+    $conn->close();
 }
 else {
     echo "No data posted with HTTP POST.";
 }
+?>
