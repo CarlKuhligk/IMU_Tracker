d45c16e38cf939f18286d3adfb8de48ebf1be347
CarlKuhligk <87583841+CarlKuhligk@users.noreply.github.com>
2021-07-22 22:33:54 +0200

    replaced with sql procedures
    gets automaticly created and initialised while adding a new device

--- web/debug/initialiseMeasreTableTest.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'http://127.0.0.1/imutracker/api/initialiseMeasureTable.php'
-myData = {"api_key":"kzNABRcbVBQghFDC"}
-
-x = requests.post(url, data = myData)
-
-print(myData)
-print(x.text)
