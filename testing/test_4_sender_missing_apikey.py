afe9c0203a0365582637bb9ffbd6fdc61e887293
CarlKuhligk <87583841+CarlKuhligk@users.noreply.github.com>
2021-10-21 12:51:21 +0200

    added websocket-tests
    added basic tests for running ratchet websocket

---
+++ testing/test_4_sender_missing_apikey.py
@@ -0,0 +1,19 @@
+import websocket
+import json
+
+
+def test(ws):
+    wsMsg = {"type": "sender"}
+    ws.send(json.dumps(wsMsg))
+    message = json.loads(ws.recv())
+    result = ""
+    if message["type"] == "response":
+        if message["id"] == "19":
+            result = "type: " + message["type"] + " id: " + message["id"]
+            return True, result
+        else:
+            result = "wrong response-id -> " + message["id"]
+            return False, result
+    else:
+        result = "wrong type -> " + message["type"]
+        return False, result
