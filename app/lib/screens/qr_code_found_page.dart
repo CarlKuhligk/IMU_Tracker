f408b6b818f367fc1158fa6ea83a864b8522b3ee
MarcusNagel <nagel_marcus@gmx.com>
2022-01-12 21:20:18 +0100

    Create qr_code_found_page.dart

---
+++ app/lib/screens/qr_code_found_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:imu_tracker/screens/main_page.dart';
+import 'package:imu_tracker/service_locator.dart';
+import 'package:imu_tracker/services/websocket_handler.dart';
+
+class QrCodeFoundPage extends StatefulWidget {
+  const QrCodeFoundPage({Key? key}) : super(key: key);
+
+  @override
+  State<QrCodeFoundPage> createState() => _QrCodeFoundPageState();
+}
+
+class _QrCodeFoundPageState extends State<QrCodeFoundPage> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Found QR-Code'),
+      ),
+      body: Center(
+        child: Column(
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Korrektes QR-Code Fromat',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
