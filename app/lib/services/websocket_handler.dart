9d8b2e9dd47dabffb8bdb73c2f9d41a7c1d4a178
MarcusNagel <nagel_marcus@gmx.com>
2022-01-12 20:40:43 +0100

    Implemented ConnectWebSocket function in WebSocketHandler
    implemented a connect function for the websocket

--- app/lib/services/websocket_handler.dart
+++ app/lib/services/websocket_handler.dart
@@ -3,9 +3,16 @@ import 'package:imu_tracker/services/localstorage_service.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:imu_tracker/services/login_data_handling.dart';
 
+import 'package:web_socket_channel/io.dart';
+import 'package:imu_tracker/data_structures/function_return_types.dart';
+import 'package:imu_tracker/data_structures/response_types.dart';
 
 class WebSocketHandler {
 //Websocket Variables
+  var sucessfullyRegistered = false;
+  late WebSocketChannel channel; //initialize a websocket channel
+  bool isWebsocketRunning = false; //status of a websocket
+  int retryLimit = 3;
 
   //static late WebSocketService _instance;
   static WebSocketChannel channel =
@@ -29,6 +36,32 @@ class WebSocketHandler {
       gyroscopeX,
       gyroscopeY,
       gyroscopeZ;
+  void connectWebSocket(socketData) async {
+    if (isWebsocketRunning) return; //check if its already running
+    this.channel = IOWebSocketChannel.connect(
+      Uri.parse('ws://${socketData['ServerIp']}'),
+    );
+    channel.stream.listen(
+      (message) {
+        var handledMessage = messageHandler(message);
+        if (handledMessage.hasMessageRightFormat &&
+            handledMessage.webSocketResponseType ==
+                responseList['deviceRegistered']!.responseNumber) {
+          isWebsocketRunning = true;
+        } //pass a function to use the recieved JSON data and parse it
+      },
+      onDone: () {
+        isWebsocketRunning = false;
+      },
+      onError: (err) {
+        isWebsocketRunning = false;
+        if (retryLimit > 0) {
+          retryLimit--;
+          connectWebSocket(socketData);
+        }
+      },
+    );
+  }
 
   void sendMessage(messageString) {
     if (messageString.isNotEmpty) {
