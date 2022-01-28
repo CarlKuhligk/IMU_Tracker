8529b499fd2385ac47b256635dcac9dbb4fef2e8
CarlKuhligk <87583841+CarlKuhligk@users.noreply.github.com>
2022-01-28 11:03:12 +0100

    Update SocketController.php
    reorganize code, split in functions

--- web/php/SocketController.php
+++ web/php/SocketController.php
@@ -61,139 +61,7 @@ class SocketController implements MessageComponentInterface
         }
 
         // handle incoming data
-        switch ($data->type) {
-            case "login":
-                if (isset($data->apikey)) {
-                    // apikey check
-                    if ($device = $this->Database->validateApiKey($data->apikey)) {
-                        // check double registration
-                        if (!$this->devices[$device->id]->online) {
-                            //check subscription
-                            if (!$this->devices[$device->id]->isObserver($from->resourceId)) {
-                                $this->devices[$device->id]->setSender($from->resourceId);
-                                $this->Database->setDeviceOnlineState($device->id, true);
-                                $from->send($this->response(RP_DEVICE_REGISTERED));
-                                // send global update
-                                $this->sendGlobalMessage($this->getDeviceInfo($device->id), SGMF_SUBSCRIBER);
-                            } else {
-                                $from->send($this->response(RP_SUBSCRIBER_CANT_REGISTER_AS_DEVICE));
-                            }
-                        } else {
-                            $from->send($this->response(RP_DEVICE_ALREADY_REGISTERED));
-                        }
-                    } else {
-                        $from->send($this->response(RP_INVALID_API_KEY));
-                    }
-                } else {
-                    $from->send($this->response(RP_MISSING_API_KEY));
-                }
-                break;
-            case "logout":
-                if (isset($data->pin)) {
-                    $authenticated = false;
-                    $callingDevice = NULL;
-                    foreach ($this->devices as $device) {
-                        // check if the senders recource id has been logedin
-                        if ($device->isSender($from->resourceId)) {
-                            $authenticated = true;
-                            $callingDevice = $device;
-                            break;
-                        }
-                    }
-                    if ($authenticated) {
-                        // logout
-                        // check pni
-                        $employee = $this->Database->validatePin($data->pin, $callingDevice);
-                        if ($employee) {
-                            // logout device
-                            $this->devices[$device->id]->unsetSender($from->resourceId);
-                            $this->Database->setDeviceOnlineState($device->id, false);
-                            $from->send($this->response(RP_DEVICE_LOGGED_OUT));
-                            // send global update
-                            $this->sendGlobalMessage($this->getDeviceInfo($device->id), SGMF_SUBSCRIBER);
-
-                            echo "Device: " . $callingDevice->id . " logged out from: " . $employee->name;
-                        } else {
-                            // wrong pin
-                            $from->send($this->response(RP_DEVICE_LOGOUT_FAILED));
-                        }
-                    } else {
-                        // cant logout
-                        $from->send($this->response(RP_DEVICE_NOT_REGISTERED));
-                    }
-                }
-                break;
-            case "subscribe":
-                if (isset($data->device_id)) {
-                    // device id check
-                    if ($device = $this->Database->validateChannelId($data->device_id)) {
-                        if (isset($data->state)) {
-                            // check is sender
-                            if (!$this->devices[$device->id]->isSender($from->resourceId)) {
-                                // observe or not observe?
-                                // !!! UGLY !!!
-                                //  |        |
-                                // \ /      \ /
-                                //  v        v
-                                if ($data->state) {
-                                    // observe
-                                    if ($this->devices[$device->id]->addObserver($from->resourceId)) {
-                                        $from->send($this->response(RP_SUBSCRIBER_REGISTERED));
-                                        $this->updateDeviceObserverCount($device->id);
-                                    } else {
-                                        $from->send($this->response(RP_SUBSCRIBER_ALREADY_REGISTERED));
-                                    }
-                                } else {
-                                    // not observe
-                                    if ($this->devices[$device->id]->removeObserver($from->resourceId)) {
-                                        $from->send($this->response(RP_SUBSCRIBER_UNREGISTERED));
-                                        $this->updateDeviceObserverCount($device->id);
-                                    } else {
-                                        $from->send($this->response(RP_SUBSCRIBER_NOT_REGISTERED));
-                                    }
-                                }
-                                //  ᴧ        ᴧ
-                                // / \      / \
-                                //  |        |
-                            } else {
-                                $from->send($this->response(RP_DEVICE_CANT_REGISTER_AS_SUBSCRIBER));
-                            }
-                        } else {
-                            $from->send($this->response(RP_SUBSCRIBER_MISSING_REGESTRATION_STATE));
-                        }
-                    } else {
-                        $from->send($this->response(RP_INVALID_DEVICE_ID));
-                    }
-                } else {
-                    $from->send($this->response(RP_MISSING_DEVICE_ID));
-                }
-                break;
-            case "data":
-                for ($i = 0; $i < 8; $i++) {
-                    // check if is empty or not a number
-                    if ($data->value[$i] == null || !is_numeric($data->value[$i] + 0)) {
-                        $from->send($this->response(RP_MISSING_DATA));
-                        return;
-                    }
-                }
-                $authenticated = false;
-                foreach ($this->devices as $device) {
-                    // check if the senders recource id has been authenticated
-                    if ($device->isSender($from->resourceId)) {
-                        $authenticated = true;
-                        // write values in database
-                        $this->Database->writeDeviceData($device->dataTableName, $data->value);
-                        // send values to all "device/device" subscribers
-                        $this->devices[$device->id]->send($data->value);
-                    }
-                }
-                if (!$authenticated) {
-                    $from->send($this->response(RP_DEVICE_NOT_REGISTERED));
-                }
-                break;
-            default:
-                $from->send($this->response(RP_UNKNOWN_DATA_TYPE));
-        }
+        $this->handleData($from, $data);
     }
 
     public function onClose(ConnectionInterface $clientConnection)
@@ -223,6 +91,176 @@ class SocketController implements MessageComponentInterface
         echo "An error has occurred: {$e->getMessage()}\n";
         $conn->close();
     }
+
+
+    public function handleData(ConnectionInterface $from, $data)
+    {
+        switch ($data->type) {
+            case "login":
+                $this->handleLogin($from, $data);
+                break;
+            case "logout":
+                $this->handleLogout($from, $data);
+                break;
+            case "subscribe":
+                $this->handleSubscription($from, $data);
+                break;
+            case "data":
+                $this->handleTrackingInformations($from, $data);
+                break;
+            default:
+                $from->send($this->response(RP_UNKNOWN_DATA_TYPE));
+        }
+    }
+
+    private function handleLogin(ConnectionInterface $from, $data)
+    {
+        if (isset($data->apikey)) {
+            // apikey check
+            if ($device = $this->Database->validateApiKey($data->apikey)) {
+                // check double registration
+                if (!$this->devices[$device->id]->online) {
+                    //check subscription
+                    if (!$this->devices[$device->id]->isObserver($from->resourceId)) {
+                        $this->devices[$device->id]->setSender($from->resourceId);
+                        $this->Database->setDeviceOnlineState($device->id, true);
+                        $from->send($this->response(RP_DEVICE_REGISTERED));
+                        // send global update
+                        $this->sendGlobalMessage($this->getDeviceInfo($device->id), SGMF_SUBSCRIBER);
+                    } else {
+                        $from->send($this->response(RP_SUBSCRIBER_CANT_REGISTER_AS_DEVICE));
+                    }
+                } else {
+                    $from->send($this->response(RP_DEVICE_ALREADY_REGISTERED));
+                }
+            } else {
+                $from->send($this->response(RP_INVALID_API_KEY));
+            }
+        } else {
+            $from->send($this->response(RP_MISSING_API_KEY));
+        }
+    }
+
+
+    private function handleLogout(ConnectionInterface $from, $data)
+    {
+        if (isset($data->pin)) {
+            $authenticated = false;
+            $callingDevice = NULL;
+            foreach ($this->devices as $device) {
+                // check if the senders recource id has been logedin
+                if ($device->isSender($from->resourceId)) {
+                    $authenticated = true;
+                    $callingDevice = $device;
+                    break;
+                }
+            }
+            if ($authenticated) {
+                // logout
+                // check pni
+                $employee = $this->Database->validatePin($data->pin, $callingDevice);
+                if ($employee) {
+                    // logout device
+                    $this->devices[$device->id]->unsetSender($from->resourceId);
+                    $this->Database->setDeviceOnlineState($device->id, false);
+                    $from->send($this->response(RP_DEVICE_LOGGED_OUT));
+                    // send global update
+                    $this->sendGlobalMessage($this->getDeviceInfo($device->id), SGMF_SUBSCRIBER);
+
+                    echo "Device: " . $callingDevice->id . " logged out from: " . $employee->name;
+                } else {
+                    // wrong pin
+                    $from->send($this->response(RP_DEVICE_LOGOUT_FAILED));
+                }
+            } else {
+                // cant logout
+                $from->send($this->response(RP_DEVICE_NOT_REGISTERED));
+            }
+        }
+    }
+
+
+
+    private function handleSubscription(ConnectionInterface $from, $data)
+    {
+        if (isset($data->device_id)) {
+            // device id check
+            if ($device = $this->Database->validateChannelId($data->device_id)) {
+                if (isset($data->state)) {
+                    // check is sender
+                    if (!$this->devices[$device->id]->isSender($from->resourceId)) {
+                        // observe or not observe?
+                        // !!! UGLY !!!
+                        //  |        |
+                        // \ /      \ /
+                        //  v        v
+                        if ($data->state) {
+                            // observe
+                            if ($this->devices[$device->id]->addObserver($from->resourceId)) {
+                                $from->send($this->response(RP_SUBSCRIBER_REGISTERED));
+                                $this->updateDeviceSubscriberCount($device->id);
+                            } else {
+                                $from->send($this->response(RP_SUBSCRIBER_ALREADY_REGISTERED));
+                            }
+                        } else {
+                            // not observe
+                            if ($this->devices[$device->id]->removeObserver($from->resourceId)) {
+                                $from->send($this->response(RP_SUBSCRIBER_UNREGISTERED));
+                                $this->updateDeviceSubscriberCount($device->id);
+                            } else {
+                                $from->send($this->response(RP_SUBSCRIBER_NOT_REGISTERED));
+                            }
+                        }
+                        //  ᴧ        ᴧ
+                        // / \      / \
+                        //  |        |
+                    } else {
+                        $from->send($this->response(RP_DEVICE_CANT_REGISTER_AS_SUBSCRIBER));
+                    }
+                } else {
+                    $from->send($this->response(RP_SUBSCRIBER_MISSING_REGESTRATION_STATE));
+                }
+            } else {
+                $from->send($this->response(RP_INVALID_DEVICE_ID));
+            }
+        } else {
+            $from->send($this->response(RP_MISSING_DEVICE_ID));
+        }
+    }
+
+
+    private function handleTrackingInformations(ConnectionInterface $from, $data)
+    {
+        if (sizeof($data->value) == 8) {
+            for ($i = 0; $i < 8; $i++) {
+                // check if is empty or not a number
+                if ($data->value[$i] == null || !is_numeric($data->value[$i] + 0)) {
+                    $from->send($this->response(RP_MISSING_DATA));
+                    return;
+                }
+            }
+        } else {
+            $from->send($this->response(PR_DATA_ITEM_CONUT_DOESNT_MATCH));
+        }
+
+        $authenticated = false;
+        foreach ($this->devices as $device) {
+            // check if the senders recource id has been authenticated
+            if ($device->isSender($from->resourceId)) {
+                $authenticated = true;
+                //check limits / events
+                //# TO DO #############################################################################################################################
+                // write values in database
+                $this->Database->writeDeviceData($device->dataTableName, $data->value, $device->state);
+                // send values to all "device/device" subscribers
+                $this->devices[$device->id]->send($data->value);
+            }
+        }
+        if (!$authenticated) {
+            $from->send($this->response(RP_DEVICE_NOT_REGISTERED));
+        }
+    }
+
 
     // send to all client is default without flags
     private function sendGlobalMessage($message, $target = 0)
@@ -281,7 +319,7 @@ class SocketController implements MessageComponentInterface
         return json_encode($response);
     }
 
-    private function updateDeviceObserverCount($id)
+    private function updateDeviceSubscriberCount($id)
     {
         $this->Database->setObserverCount($id, $this->devices[$id]->observerCount);
         $this->sendGlobalMessage($this->getDeviceInfo($id), SGMF_SUBSCRIBER);
