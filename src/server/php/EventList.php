8fdb2f79dbee2a4c575d3e8372f801b2e78672b0
CarlKuhligk <87583841+CarlKuhligk@users.noreply.github.com>
2022-04-03 21:26:46 +0200

    Create EventList.php

---
+++ src/server/php/EventList.php
@@ -0,0 +1,9 @@
+<?php
+# events
+define("E_BATTERY_LOW", 10);
+define("E_BATTERY_EMPTY", 11);
+define("E_CONNECTED", 20);
+define("E_CONNECTION_LOST", 21);
+define("E_CONNECTION_TIMEOUT", 22);
+define("E_ACCELERATION_LIMIT_EXCEEDED", 30);
+define("E_ROTATION_LIMIT_EXCEEDED", 31);
