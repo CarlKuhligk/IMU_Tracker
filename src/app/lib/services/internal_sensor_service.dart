777fdfe22c363ee4e0c2843b6219a5c55a661af8
MarcusNagel <nagel_marcus@gmx.com>
2022-04-08 11:17:12 +0200

    Update internal_sensor_service.dart
    - routines for acceleration and gyroscopesensor listeners
    - routine for measurement interval
    - routines for magnitude calculation

--- src/app/lib/services/internal_sensor_service.dart
+++ src/app/lib/services/internal_sensor_service.dart
@@ -2,9 +2,11 @@
 //dart packages
 
 import 'dart:async';
+import 'dart:math';
 
 import 'package:sensors/sensors.dart';
 import 'package:battery_plus/battery_plus.dart';
+import 'package:environment_sensors/environment_sensors.dart';
 
 class InternalSensorService {
   final Battery _battery = Battery();
@@ -14,23 +16,26 @@ class InternalSensorService {
 
   StreamSubscription? accelerationSubscription;
   StreamSubscription? gyroscopeSubscription;
-  Timer? timer;
-  UserAccelerometerEvent? event;
+  Timer? measurementIntervalTimer;
 
-  var accelerationValues;
-  var gyroscopeValues;
+  var magnitudeAccelerometer;
+  var magnitudeGyroscope;
+  var _accelerationValues;
+  var _gyroscopeValues;
+  var batteryLevel;
 
   startInternalSensors() {
-    startGyroscopeSensor();
-    startAccelerationSensor();
-    startBatterySensor();
+    _startGyroscopeSensor();
+    _startAccelerationSensor();
+    _startBatterySensor();
+    _startMeasurementInterval();
   }
 
-  startGyroscopeSensor() {
+  _startGyroscopeSensor() {
     // if the gyroscope subscription hasn't been created, go ahead and create it
     if (gyroscopeSubscription == null) {
       gyroscopeSubscription = gyroscopeEvents.listen((GyroscopeEvent eve) {
-        gyroscopeValues = eve;
+        _gyroscopeValues = eve;
       });
     } else {
       // it has already ben created so just resume it
@@ -38,12 +43,12 @@ class InternalSensorService {
     }
   }
 
-  startAccelerationSensor() {
+  _startAccelerationSensor() {
     // if the accelerometer subscription hasn't been created, go ahead and create it
     if (accelerationSubscription == null) {
       accelerationSubscription =
           userAccelerometerEvents.listen((UserAccelerometerEvent eve) {
-        accelerationValues = eve;
+        _accelerationValues = eve;
       });
     } else {
       // it has already ben created so just resume it
@@ -51,7 +56,7 @@ class InternalSensorService {
     }
   }
 
-  startBatterySensor() {
+  _startBatterySensor() {
     // if the battery subscription hasn't been created, go ahead and create it
     if (_batteryStateSubscription == null) {
       _batteryStateSubscription =
@@ -62,5 +67,36 @@ class InternalSensorService {
       // it has already ben created so just resume it
       _batteryStateSubscription?.resume();
     }
+  }
+
+  _startMeasurementInterval() {
+    if (measurementIntervalTimer == null ||
+        !measurementIntervalTimer!.isActive) {
+      measurementIntervalTimer =
+          Timer.periodic(const Duration(milliseconds: 200), (_) {
+        //TODO: get measurement interval from settings
+        _getBatteryPercentage();
+        _calculateGyroscopeMagnitude();
+        _calculateAccelerometerMagnitude();
+      });
+    }
+  }
+
+  _getBatteryPercentage() async {
+    batteryLevel = await _battery.batteryLevel;
+  }
+
+  _calculateGyroscopeMagnitude() {
+    var rawData = _gyroscopeValues;
+    magnitudeGyroscope = sqrt((rawData.x * rawData.x) +
+        (rawData.y * rawData.y) +
+        (rawData.z * rawData.z));
+  }
+
+  _calculateAccelerometerMagnitude() {
+    var rawData = _accelerationValues;
+    magnitudeAccelerometer = sqrt((rawData.x * rawData.x) +
+        (rawData.y * rawData.y) +
+        (rawData.z * rawData.z));
   }
 }
