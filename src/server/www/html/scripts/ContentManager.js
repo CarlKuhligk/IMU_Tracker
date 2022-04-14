3bb43ee8afb6933c52fc898dbcf276dc8720830f
CarlKuhligk <87583841+CarlKuhligk@users.noreply.github.com>
2022-04-14 17:46:21 +0200

    basic webinterface

---
+++ src/server/www/html/scripts/ContentManager.js
@@ -0,0 +1,114 @@
+export class ContentManager {
+  constructor() {
+    this.navigation = document.createElement("div");
+    this.navigation.classList.add("navigation");
+
+    this.navigationHead = document.createElement("div");
+    this.navigationHead.classList.add("navigationHead");
+    this.navigationHeadLabel1 = document.createElement("label");
+    this.navigationHeadLabel1.textContent = "Security Motion Tracker";
+
+    this.navigationEventSection = document.createElement("div");
+    this.navigationEventSection.classList.add("navigationSection");
+    this.navigationEventSection.id = "eventNavigation";
+    this.navigationEventSectionButton1 = document.createElement("button");
+    this.navigationEventSectionButton1.innerText = "Events";
+
+    this.navigationDeviceSection = document.createElement("div");
+    this.navigationDeviceSection.classList.add("navigationSection");
+    this.navigationDeviceSection.id = "deviceNavigation";
+    this.navigationDeviceSectionLabel1 = document.createElement("label");
+    this.navigationDeviceSectionLabel1.innerText = "Geräte";
+
+    this.navigationDeviceSectionDeviceList = document.createElement("div");
+    this.navigationDeviceSectionDeviceList.classList.add(
+      "navigationDevicesSubSection"
+    );
+    this.navigationDeviceSection.id = "deviceList";
+
+    this.buildNavigationHTML();
+  }
+
+  buildNavigationHTML() {
+    this.navigationHead.appendChild(this.navigationHeadLabel1);
+    this.navigation.appendChild(this.navigationHead);
+
+    this.navigationEventSection.appendChild(this.navigationEventSectionButton1);
+    this.navigation.appendChild(this.navigationEventSection);
+
+    this.navigationDeviceSection.appendChild(
+      this.navigationDeviceSectionLabel1
+    );
+    this.navigationDeviceSection.appendChild(
+      this.navigationDeviceSectionDeviceList
+    );
+
+    this.navigation.appendChild(this.navigationDeviceSection);
+
+    $(".rootNavigation").empty();
+    $(".rootNavigation").append(this.navigation);
+  }
+
+  generateDeviceHTMLObject(device) {
+    var newDeviceNavigationObject = document.createElement("div");
+    newDeviceNavigationObject.classList.add("device");
+    newDeviceNavigationObject.id = device.id;
+
+    // add connection image
+    var imgConnection = document.createElement("img");
+    imgConnection.classList.add("isConnected");
+    if (device.isConnected === 1) {
+      imgConnection.src = "./img/circle-green.png";
+      imgConnection.alt = "connected";
+    } else {
+      imgConnection.src = "./img/circle-grey.png";
+      imgConnection.alt = "not connected";
+    }
+    newDeviceNavigationObject.appendChild(imgConnection);
+
+    // add id label
+    var labelDeviceIs = document.createElement("label");
+    //_____labelChannelName.onclick = onClickModuleLabel;
+    labelDeviceIs.innerText = device.id;
+    newDeviceNavigationObject.appendChild(labelDeviceIs);
+
+    // add employee label
+    var labelEmployee = document.createElement("label");
+    //_____labelChannelName.onclick = onClickModuleLabel;
+    labelEmployee.innerText = device.employee;
+    newDeviceNavigationObject.appendChild(labelEmployee);
+
+    // add battery label
+    var labelBattery = document.createElement("label");
+    //_____labelChannelName.onclick = onClickModuleLabel;
+    labelBattery.innerText = "???";
+    newDeviceNavigationObject.appendChild(labelBattery);
+
+    // add event info image
+    var imgEventInfo = document.createElement("img");
+    imgEventInfo.classList.add("eventInfo");
+    imgEventInfo.src = "./img/information-variant.png";
+    imgEventInfo.alt = "event information";
+    newDeviceNavigationObject.appendChild(imgEventInfo);
+
+    return newDeviceNavigationObject;
+  }
+
+  addDeviceEntity(device) {
+    $(".navigationDevicesSubSection").append(
+      this.generateDeviceHTMLObject(device)
+    );
+  }
+
+  removeDeviceEntity(device) {
+    var selection = "#" + device.id;
+    $(selection).remove();
+  }
+
+  updateDeviceEntity(device) {
+    this.removeDeviceEntity(device);
+    $(".navigationDevicesSubSection").prepend(
+      this.generateDeviceHTMLObject(device)
+    );
+  }
+}
---
+++ src/server/www/html/scripts/MessageManager.js
@@ -0,0 +1,89 @@
+export class MessageManager {
+  constructor() {
+    this.listeners = {};
+  }
+
+  connect() {
+    // create a new WebSocket.
+    this.websocket = new WebSocket("ws://192.168.212.9:8080");
+
+    this.websocket.addEventListener("open", (event) => {
+      this.onOpen(event);
+    });
+
+    this.websocket.addEventListener("message", (event) => {
+      this.onMessage(event);
+    });
+
+    this.websocket.addEventListener("close", (event) => {
+      this.onClose(event);
+    });
+  }
+
+  onOpen(event) {
+    var messageObject = {};
+    messageObject.t = "s";
+    messageObject.s = 1;
+    var subscribeMessage = JSON.stringify(messageObject);
+    this.websocket.send(subscribeMessage);
+  }
+
+  onMessage(event) {
+    var message = JSON.parse(event.data);
+
+    switch (message.t) {
+      case "uc":
+        console.log("UpdateConnection received: %o", message);
+        this.triggerEvent("handleUpdateConnection", message);
+        break;
+
+      case "M":
+        console.log("Add measurement received: %o", message);
+        this.triggerEvent("handleAddEvent", message);
+        break;
+
+      case "e":
+        console.log("Add event received: %o", message);
+        message.d.forEach((element) => {
+          this.triggerEvent("handleAddEvent", element);
+        });
+        break;
+
+      case "su":
+        console.log("Settings update received: %o", message);
+        this.triggerEvent("handleSettingsUpdate", message);
+        break;
+
+      case "ad":
+        console.log("Add device received: %o", message);
+        message.d.forEach((element) => {
+          this.triggerEvent("handleAddDevice", element);
+        });
+        break;
+
+      case "rd":
+        console.log("Remove device received: %o", message);
+        this.triggerEvent("handleRemoveDevice", message);
+        break;
+    }
+  }
+
+  onClose(e) {
+    console.log("Connection closed!");
+  }
+
+  addEventListener(method, callback) {
+    this.listeners[method] = callback;
+  }
+
+  removeEventListener(method) {
+    delete this.listeners[method];
+  }
+
+  triggerEvent(method, payload = null) {
+    const callback = this.listeners[method];
+    if (typeof callback === "function") {
+      callback(payload);
+    }
+  }
+}
