0fb4939ccd6b42892db18456aba4fe7b8209f1d6
CarlKuhligk <87583841+CarlKuhligk@users.noreply.github.com>
2022-04-16 23:05:32 +0200

    Update main.js
    added add event methode

--- src/server/www/html/scripts/main.js
+++ src/server/www/html/scripts/main.js
@@ -1,13 +1,13 @@
 import { Device } from "./Device.js";
 import { MessageManager } from "./MessageManager.js";
 import { NavigationManager } from "./NavigationManager.js";
-import { ContentManager } from "./ContentManager.js";
 
 var messageManager = new MessageManager();
 var navigationManager;
-var content;
 
 var deviceList = [];
+Device.events = [];
+Device.content = "";
 
 function bodyLoad() {
   messageManager.connect();
@@ -29,12 +29,20 @@ function handleUpdateConnection(updateMessage) {
   navigationManager.updateDeviceEntity(deviceList[updateMessage.i]);
 }
 
+function handleAddEvent(addEventMessage) {
+  Device.addEvent(addEventMessage);
+}
+
 messageManager.addEventListener("handleAddDevice", (event) => {
   handleAddDevice(event);
 });
 
 messageManager.addEventListener("handleUpdateConnection", (event) => {
   handleUpdateConnection(event);
+});
+
+messageManager.addEventListener("handleAddEvent", (event) => {
+  handleAddEvent(event);
 });
 
 window.addEventListener("load", bodyLoad());
